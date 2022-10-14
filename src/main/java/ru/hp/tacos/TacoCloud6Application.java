844baec0a1ce327da012b576096a19b116aad562
“Sherstobitov” <sherstobitov@finexcons.ru>
2022-10-14 09:44:07 +0500

    TacoCloud initialization

---
+++ src/main/java/ru/hp/tacos/TacoCloud6Application.java
@@ -0,0 +1,13 @@
+package ru.hp.tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloud6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloud6Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/hp/tacos/TacoCloud6ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hp.tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloud6ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
